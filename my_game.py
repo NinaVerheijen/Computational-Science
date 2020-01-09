8c5d761d36928660e7856aa41276c9217c6778e8
Nina Verheijen <nina.verheijen@student.uva.nl>
2020-01-09 14:53:28 +0100

    Create my_game.py

---
+++ my_game.py
@@ -0,0 +1,76 @@
+import pygame
+import sys
+import math
+from pygame import *
+from pygame.locals import *
+from pygame.sprite import *
+
+
+pygame.init()
+
+
+
+WIDTH = 1100
+HEIGHT = 400
+CAPTION = 'Traffic Simulator'
+
+speed = 0
+max_speed = 30
+velocity = [10,10]
+
+
+class Block(pygame.sprite.Sprite):
+
+    # Constructor. Pass in the color of the block,
+    # and its x and y position
+    def __init__(self, color, width, height, x, y):
+       # Call the parent class (Sprite) constructor
+       pygame.sprite.Sprite.__init__(self)
+
+       # Create an image of the block, and fill it with a color.
+       # This could also be an image loaded from the disk.
+       self.image = pygame.Surface([width, height])
+       self.image.fill(color)
+       self.x = int(x)  # variable denoting x position of car
+       self.y = int(y)
+
+       # Fetch the rectangle object that has the dimensions of the image
+       # Update the position of this object by setting the values of rect.x and rect.y
+       self.rect = self.image.get_rect()
+
+    def move(self, xp, a):
+
+        xp = self.x+xp  # new place for the car
+
+        if xp > 1200:
+            xp = xp - 1200
+        dista = xp-a[0]
+
+        self.rect.left = xp  # move the car
+        self.x = xp  # update the car position
+        
+
+def traffic():
+    frame = pygame.display.set_mode((WIDTH, HEIGHT))
+    xp = 100
+    car1 = Block((255,0,0),5,5, xp+0, 200)
+    all_cars = Group(car1)
+
+
+    while True:
+        xp = 2
+        car1.move(xp, [xp+200,200])
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+
+        frame.fill((255, 255, 255))
+        all_cars.draw(frame)
+        display.update()
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    traffic()
