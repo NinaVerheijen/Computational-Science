a26a0ea9a9cde863f8838b82d457b11dc004257d
Nina Verheijen <nina.verheijen@student.uva.nl>
2020-01-10 14:22:30 +0100

    Update my_game.py

--- my_game.py
+++ my_game.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import math
 import random
+import time
 from pygame import *
 from pygame.locals import *
 from pygame.sprite import *
@@ -15,58 +16,71 @@ HEIGHT = 400
 CAPTION = 'Traffic Simulator'
 
 speed = 0
-max_speed = 30
-velocity = [0.5, 0.5]
+max_speed = 2 #130 km\h
 
 
 class Block(pygame.sprite.Sprite):
 
     # Constructor. Pass in the color of the block,
     # and its x and y position
-    def __init__(self, color, width, height, x, y):
+    def __init__(self, color, size, x, y, speed, direction):
        # Call the parent class (Sprite) constructor
        pygame.sprite.Sprite.__init__(self)
 
        # Create an image of the block, and fill it with a color.
        # This could also be an image loaded from the disk.
-       self.image = pygame.Surface([width, height])
+       self.image = pygame.Surface(size)
        self.image.fill(color)
+       self.speed  = speed
        self.x = int(x)  # variable denoting x position of car
        self.y = int(y)
+       self.direction = direction
 
        # Fetch the rectangle object that has the dimensions of the image
        # Update the position of this object by setting the values of rect.x and rect.y
        self.rect = self.image.get_rect(center=(x,y))
 
-    def move(self, velocity, a):
+    def move(self):
 
-        new_x = self.x + velocity[0]  # new place for the car
-        new_y = self.y + velocity[1]
+        new_x = self.x + self.speed  # new place for the car
+        new_y = self.y + self.direction[1]
 
-        # if velocity[0] > WIDTH:
+        # if direction[0] > WIDTH:
         #     new_x = new_x - WIDTH
-        # if velocity[0] > HEIGHT:
+        # if direction[0] > HEIGHT:
         #     new_y = new_y - HEIGHT
         #dista = xp-a[0]
 
-        self.rect=self.rect.move(velocity[0],velocity[1]) # Move the car with the given velocity
-        # self.rect.left = new_x  # move the car
-        # self.x = new_x  # update the car position
-        # self.rect.left = new_y  # move the car
-        # self.y = new_y
+
+        # self.rect=self.rect.move(direction[0],direction[1]) # Move the car with the given direction
+        self.rect.right = new_x  # move the car
+        self.x = new_x  # update the car position
+        self.rect.bottom = new_y  # move the car
+        self.y = new_y
 
 
 def traffic():
     frame = pygame.display.set_mode((WIDTH, HEIGHT))
-    car1 = Block((255, 0, 0), 10, 10, velocity[0] + 100, random.randrange(0,200,1))
-    all_cars = Group(car1)
+
+
+    car1 = Block((255, 0, 0), [10, 10], 100, random.randrange(0,200,1), 0, [0.2,0])
+    car2 = Block((0, 255, 0), [10, 10], + 100, random.randrange(0, 200, 1), 0, [0.5,0])
+    all_cars = Group(car1, car2)
 
     while True:
-        car1.move(velocity, [100, 100])
-        # if velocity[0] > 10:
-        #     velocity[0] = -2
+
+        for car in all_cars:
+            if car.speed < max_speed:
+                car.speed += 0.0001
+            car.move()
+
+        if pygame.sprite.collide_rect(car1, car2):
+            car2.direction[0] -= 0.4
+
+        # if direction[0] > 10:
+        #     direction[0] = -2
         # else:
-        #     velocity[0] += 1
+        #     direction[0] += 1
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
