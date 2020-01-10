add489a10757782d87e6e4a49c8df0902a355e7d
Guilly Kolkman <gkolkm@gmail.com>
2020-01-10 13:08:37 +0100

    random car generating

--- my_game.py
+++ my_game.py
@@ -19,24 +19,24 @@ max_speed = 30
 velocity = [2, 0]
 
 
-class Block(pygame.sprite.Sprite):
+class Vehicle(pygame.sprite.Sprite):
 
-    # Constructor. Pass in the color of the block,
+    # Constructor. Pass in the color of the Vehicle,
     # and its x and y position
-    def __init__(self, color, width, height, x, y):
-       # Call the parent class (Sprite) constructor
-       pygame.sprite.Sprite.__init__(self)
+    def __init__(self, ID, color, width, height, x, y):
+        # Call the parent class (Sprite) constructor
+        pygame.sprite.Sprite.__init__(self)
+        self.ID = ID
+        # Create an image of the Vehicle, and fill it with a color.
+        # This could also be an image loaded from the disk.
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+        self.x = int(x)  # variable denoting x position of car
+        self.y = int(y)
 
-       # Create an image of the block, and fill it with a color.
-       # This could also be an image loaded from the disk.
-       self.image = pygame.Surface([width, height])
-       self.image.fill(color)
-       self.x = int(x)  # variable denoting x position of car
-       self.y = int(y)
-
-       # Fetch the rectangle object that has the dimensions of the image
-       # Update the position of this object by setting the values of rect.x and rect.y
-       self.rect = self.image.get_rect(center=(x,y))
+        # Fetch the rectangle object that has the dimensions of the image
+        # Update the position of this object by setting the values of rect.x and rect.y
+        self.rect = self.image.get_rect(center=(x,y))
 
     def move(self, velocity, a):
 
@@ -58,11 +58,17 @@ class Block(pygame.sprite.Sprite):
 
 def traffic():
     frame = pygame.display.set_mode((WIDTH, HEIGHT))
-    car1 = Block((255, 0, 0), 10, 10, velocity[0] + 100, random.randrange(0,200,1))
-    all_cars = Group(car1)
+    # car1 = Vehicle((255, 0, 0), 10, 10, velocity[0] + 100, random.randrange(0,200,1))
+    all_cars = Group()
 
     while True:
-        car1.move(velocity, [100, 100])
+        chance = random.uniform(0, 1)
+        if chance < 0.07:
+            car = Vehicle(chance, (255, 0, 0), 10, 10, velocity[0] + 0, random.randrange(5,400,35))
+            all_cars.add(car)
+        for m in all_cars:
+            m.move(velocity, [100, 100])
+        # car1.move(velocity, [100, 100])
         # if velocity[0] > 10:
         #     velocity[0] = -2
         # else:
