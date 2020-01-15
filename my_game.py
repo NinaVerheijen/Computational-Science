31c99a4b327d02e49e7cda2fe1b48e21e186d8dd
Guilly Kolkman <gkolkm@gmail.com>
2020-01-15 14:13:14 +0100

    added directory, split files

--- my_game.py
+++ my_game.py
@@ -1,3 +1,4 @@
+# from .dir import Dir
 import pygame
 import sys
 import math as Math
@@ -6,14 +7,17 @@ import time as tijd
 from pygame import *
 from pygame.locals import *
 from pygame.sprite import *
+from Vehicle import Vehicle
+from road import Road
 
+# from Vehicle import Vehicle
 
 pygame.init()
 
 clock = pygame.time.Clock()
 clock.tick(60)
 
-lanes = [lane1, lane2, lane3]
+# lanes = [lane1, lane2, lane3]
 random.seed(2)
 
 WIDTH = 1100 # 8 km?
@@ -32,100 +36,6 @@ def pixel_to_meter(pixels):
     one_p = (road_length * 1000)/WIDTH
     dist = pixels*one_p
     return dist
-
-class Vehicle(pygame.sprite.Sprite):
-
-    # Constructor. Pass in the color of the Vehicle,
-    # and its x and y position
-
-    def __init__(self, ID, color, size, x, lane, speed, direction):
-
-       # Call the parent class (Sprite) constructor
-       pygame.sprite.Sprite.__init__(self)
-
-       # Create an image of the block, and fill it with a color.
-       # This could also be an image loaded from the disk.
-       self.ID = ID
-       self.image = pygame.Surface(size)
-       self.image.fill(color)
-       self.speed  = speed
-
-       self.x = int(x)  # variable denoting x position of car
-       self.y = int(lane)
-       self.direction = direction
-       self.lane = lane / 50
-       self.size = size
-
-       # Fetch the rectangle object that has the dimensions of the image
-       # Update the position of this object by setting the values of rect.x and rect.y
-       self.rect = self.image.get_rect(center=(x,lane))
-       self.rect.size = (size[0] + 10, size[1])
-
-
-    def desired_gap(self, v, d_v):
-        """
-        s_0 is minimum bumper to bumper gap -> 2m
-        a is acceleration in every day traffic -> 0.3m/s^2
-        b is comfortable (breaking) deceleration in everyday traffic -> 3.0m/s^2
-        delta is acceleration exponent -> 4
-        T is desired safety time -> 1.5 s
-        s is current gap
-        """
-        s_0 = 300  # minimum gap between cars
-        a = 0.3
-        b = 3
-        T = 1.5                   
-        des_gap = s_0 + max(0, v*T + ((v*d_v)/ (2*Math.sqrt(a*b))))
-        return des_gap
-
-    def comp_acc(self, s, lead_speed):
-        a = 0.3
-        v_0 = max_speed
-        v = self.speed
-        d = 4
-        d_v = abs(v - lead_speed)
-        a_free = a*(1-(v/v_0)**d)
-        a_int = a*((self.desired_gap(v, d_v) / s)**2)
-    
-        acc =  a_free - a_int
-        return acc
-
-    def move(self):
-        new_x = self.x + meter_to_pixel(self.speed)  # new place for the car
-        new_y = self.y + self.direction[1]
-
-        self.rect.right = new_x  # move the car
-        self.x = new_x  # update the car position
-        self.rect.bottom = new_y  # move the car
-        self.y = new_y
-
-
-class Road:
-
-    def __init__(self, lanes, max_speed):
-
-        self.lanes = []
-        self.max_speed = max_speed
-        self.pos_lanes = []
-        
-        # Initialize the starting lanes
-        for number_lanes in range(lanes):
-            self.lanes.append([])
-            self.pos_lanes.append(50 * (number_lanes+1))
-
-    # Add new lane to the road
-    def add_lane(self):
-        self.lanes.append([])
-        self.pos_lanes.append(50 * (len(self.lanes)))
-
-    # Delete the last lane and delete all cars on that lane
-    def delete_lane(self, all_cars):
-        for delete_cars in all_cars:
-            if delete_cars.y == self.pos_lanes[-1]:
-                all_cars.remove(delete_cars)
-        self.lanes.pop()
-        self.pos_lanes.pop()
-
 
 
 def traffic():
@@ -155,7 +65,7 @@ def traffic():
             for number in range(len(road.pos_lanes)):
                 if car.y == road.pos_lanes[number]:
                     road.lanes[number].append(car)
-                    print(car.lane)
+                    # print(car.lane)
 
         for car in all_cars:
             for c in all_cars:
---
+++ road.py
@@ -0,0 +1,28 @@
+import pygame
+
+
+class Road:
+
+    def __init__(self, lanes, max_speed):
+
+        self.lanes = []
+        self.max_speed = max_speed
+        self.pos_lanes = []
+        
+        # Initialize the starting lanes
+        for number_lanes in range(lanes):
+            self.lanes.append([])
+            self.pos_lanes.append(50 * (number_lanes+1))
+
+    # Add new lane to the road
+    def add_lane(self):
+        self.lanes.append([])
+        self.pos_lanes.append(50 * (len(self.lanes)))
+
+    # Delete the last lane and delete all cars on that lane
+    def delete_lane(self, all_cars):
+        for delete_cars in all_cars:
+            if delete_cars.y == self.pos_lanes[-1]:
+                all_cars.remove(delete_cars)
+        self.lanes.pop()
+        self.pos_lanes.pop()
