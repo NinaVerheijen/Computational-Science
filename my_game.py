d9185c82142c56961c675af6999f58432cbe1584
Nynke Doornhof <nynkedoornhof@hotmail.com>
2020-01-23 13:34:35 +0100

    visuals

--- my_game.py
+++ my_game.py
@@ -6,6 +6,7 @@ import math as Math
 import random
 import bisect  
 import time as tijd
+import matplotlib.pyplot as plt
 from pygame import *
 from pygame.locals import *
 from pygame.sprite import *
@@ -15,10 +16,10 @@ from road import Road
 # from Vehicle import Vehicle
 os.environ['SDL_VIDEO_WINDOW_POS'] = '0,0'
 
-pygame.init()
+# pygame.init()
 
-clock = pygame.time.Clock()
-clock.tick(60)
+# clock = pygame.time.Clock()
+# clock.tick(60)
 
 # background 2 or 3 lanes
 
@@ -48,7 +49,11 @@ def pixel_to_meter(pixels):
     dist = pixels*one_p
     return dist
 
-def traffic():
+def traffic(maxspeed):
+    pygame.init()
+
+    clock = pygame.time.Clock()
+    clock.tick(60)
     frame = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
 
     all_cars = Group()
@@ -59,25 +64,43 @@ def traffic():
     # print(road.lanes, road.pos_lanes)
     road.delete_lane(all_cars)
     # print(road.lanes)
-
+    start_ticks=pygame.time.get_ticks()
+    trafficcount = 0
+    trafficcountie = 0
+    graphie = []
+    graphieint = []
+    timie = []
+    # graphtime interval
+    t = 2
     while True:
         tijd.sleep(0.000000000000000000000000000000000000000000000000000000001)
         chance = random.uniform(0, 1)
+        seconds=(pygame.time.get_ticks()-start_ticks)/1000
+
+
+        if int(seconds) % t == 0 and int(seconds) not in timie:
+            print(int(seconds))
+            graphie.append(trafficcount)
+            graphieint.append(trafficcountie)
+            trafficcountie = 0
+            timie.append(int(seconds))
+        # else:
+        #     print(int(seconds), seconds, trafficcount, trafficcountie)
 
         if chance < 0.3:
             truck_chance = random.uniform(0,1)
             if truck_chance < 0.80:
-                car = Vehicle(chance, 'car', (255, 0, 0), [24/2, 12/2], -50, random.choice(road.pos_lanes), 50 + random.randrange(-10,10,2), [0.2,0])
-                all_cars.add(car)
+                vehicle = Vehicle(chance, 'car', maxspeed, (255, 0, 0), [24/2, 12/2], -50, random.choice(road.pos_lanes), 50 + random.randrange(-10,10,2), [0.2,0])
+                all_cars.add(vehicle)
             else:
                 choice = random.choices(population = road.pos_lanes, weights = [0.02, 0.04, 0.1, 0.84])
-                truck = Vehicle(chance, 'truck', (0, 0, 255), [98/2, 14/2], -50, choice[0], 10 + random.randrange(-5,5,1), [0.2,0])
-                all_cars.add(truck)
+                vehicle = Vehicle(chance, 'truck', maxspeed, (0, 0, 255), [98/2, 14/2], -50, choice[0], 10 + random.randrange(-5,5,1), [0.2,0])
+                all_cars.add(vehicle)
 
             # put car in the right lane and keep track of which lane the car is
             for number in range(len(road.pos_lanes)):
-                if car.y == road.pos_lanes[number]:
-                    road.lanes[number].append(car)
+                if vehicle.y == road.pos_lanes[number]:
+                    road.lanes[number].append(vehicle)
 
         for car in all_cars:
             change_lanes = random.uniform(0, 1)
@@ -161,7 +184,9 @@ def traffic():
                 
             car.move()
             if car.x > WIDTH:
-                print(car.speed)
+                # print(car.speed)
+                trafficcount += 1
+                trafficcountie += 1
                 all_cars.remove(car)
 
         # quit pygame
@@ -171,8 +196,10 @@ def traffic():
                     pygame.quit()
 
             if event.type == pygame.QUIT:
+                # total average number of vehicles per time interval
+                trafficflow = (trafficcount / seconds) *t
                 pygame.quit()
-                exit()
+                return trafficflow, graphie, graphieint, timie
 
 
         # make pygame
@@ -182,5 +209,37 @@ def traffic():
         pygame.display.flip()
 
 
+
+
 if __name__ == '__main__':
-    traffic()
+    # run traffic lower speed
+    average_tf_l, cummulative_tf_l, interval_tf_l, timeline_l = traffic(100)
+    # run traffic higher speed
+    average_tf_h, cummulative_tf_h, interval_tf_h, timeline_h = traffic(130)
+
+    # print(timie, graphieint, graphie)
+    # print(timie1, graphieint1, graphie1)
+    # plt.plot(timie, graphie, label="100 cumm")
+    # plt.plot(timie, graphieint, label="100 int")
+    # plt.legend()
+    # # plt.show()
+    # plt.plot(timie1, graphie1, label="130 cumm")
+    # plt.plot(timie1, graphieint1, label="130 int")
+    # plt.legend()
+    # plt.show()
+    # maxspeed = 130
+    # timeinterval = 2
+    # print(traffic(maxspeed, timeinterval))
+    # print(traffic(120, timeinterval))
+
+
+
+    # plt.plot(timeline, cummulative_tf, label="Cummulative trafficflow", c="#7F98FF")
+    # plt.plot(timeline, interval_tf, label="Trafficflow per time unit", c="#3152D4")
+    # plt.plot(timeline, [average_tf] * len(timeline), label="Average trafficflow", c="#001F9A")
+    plt.plot(timeline_l, cummulative_tf_l, label="Cummulative trafficflow", c="lightblue")
+    plt.plot(timeline_l, interval_tf_l, label="Trafficflow per time unit", c="blue")
+    plt.plot(timeline_l, [average_tf_l] * len(timeline_l), label="Average trafficflow", c="darkblue")
+    plt.legend()
+    plt.show()
+    
